e917b71c315978f07e09b3631225b6235dc42d14
Prathamesh <prathamesh1729@gmail.com>
2015-03-30 21:38:17 +0000

    Added filed reading

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
+var filesys = require('fs')
 var app = express();
+var fs = filesys();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World v2!')
+  //response.send('Hello World v2!')
+  response.send(fs.readFileSync("index.html").toString())
 })
 
 app.listen(app.get('port'), function() {
